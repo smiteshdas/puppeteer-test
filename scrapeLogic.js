b8af83fb23a028ea77db695559f1c79d4a6d2496
Smitesh Das <57407736+smiteshdas@users.noreply.github.com>
2023-04-04 00:18:12 +0530

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -21,13 +21,13 @@ const scrapeLogic = async (res) => {
       await page.setViewport({width: 1080, height: 1024});
 
       await Promise.all([
-        page.goto("https://colab.research.google.com/drive/1j3CoFol2o9I-92-yic7otdhZdodieyVK", {"waitUntil":["load", "networkidle2"]}),
+        page.goto("https://www.instagram.com/handylusapp", {"waitUntil":["load", "networkidle2"]}),
        
         new Promise(async function(resolve, reject){
           //screenshot on first console message
           page.once("console", async () => {
-    const signInBtn = await page.waitForSelector("body>div[6]>div[1]>div>div[2]>div[2]>div[3]>div>div>div>div>div>a>span");
-    const fullTitle = await signInBtn.evaluate((el) => el.textContent);
+    const signInBtn = await page.waitForSelector("body>div[2]>div>div>div[1]>div>div>div>div[1]>section>main>div>div[4]>article>div>div>div[1]>div[1]>a>div>div[1]>img);
+    const fullTitle = await signInBtn.evaluate((el) => el.alt);
     
     const logStatement = `The title of this blog post is ${fullTitle}`;
     console.log(logStatement);
@@ -55,6 +55,8 @@ module.exports = { scrapeLogic };
       
       
   /*
+  
+  https://stackoverflow.com/questions/54527982/why-is-puppeteer-reporting-unhandledpromiserejectionwarning-error-navigation
   
   try {
     const page = await browser.newPage();
