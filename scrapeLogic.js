45e0e0d58bc8a57a1e118e49c3e8dd79a8f9636a
Smitesh Das <57407736+smiteshdas@users.noreply.github.com>
2023-04-03 23:49:20 +0530

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -22,8 +22,8 @@ const scrapeLogic = async (res) => {
 
       await Promise.all([
         page.goto("https://colab.research.google.com/drive/1j3CoFol2o9I-92-yic7otdhZdodieyVK", {"waitUntil":["load", "networkidle2"]}),
+       
         new Promise(async function(resolve, reject){
-
           //screenshot on first console message
           page.once("console", async () => {
     const signInBtn = await page.waitForSelector("body>div[6]>div[1]>div>div[2]>div[2]>div[3]>div>div>div>div>div>a");
@@ -35,8 +35,12 @@ const scrapeLogic = async (res) => {
             
             resolve();
           });
+         
         })
       ]);
+     
+    })
+        }
       
       await load_page()
   
@@ -60,9 +64,7 @@ module.exports = { scrapeLogic };
     // Set screen size
     await page.setViewport({ width: 1080, height: 1024 });
     
-    */
-
-   /*
+   
    // Type into search box
     await page.type(".search-box__input", "automate beyond recorder");
 
@@ -80,24 +82,24 @@ module.exports = { scrapeLogic };
     // Print the full title
     const logStatement = `The title of this blog post is ${fullTitle}`;
     console.log(logStatement);
-    res.send(logStatement);*/
+    res.send(logStatement);
     
-   /*
+   
   
     const signInBtn = await page.waitForSelector("body>div[6]>div[1]>div>div[2]>div[2]>div[3]>div>div>div>div>div>a");
     const fullTitle = await signInBtn.evaluate((el) => el.textContent);
     
-    */
+    
    
-    /*
+    
     await page.click(searchResultSelector);
 
     await page.waitForSelector(".search-box__link");    
     await page.type(".search-box__input", "automate beyond recorder");
     await page.keyboard.press('Enter')
-    */
     
-    /*
+    
+    
     const logStatement = `The title of this blog post is ${fullTitle}`;
     console.log(logStatement);
     res.send(logStatement);
